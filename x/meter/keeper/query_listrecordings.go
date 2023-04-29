eb803a4286bf723c4208158813c5f346da8e6d81
alkia <maxime@alkia.net>
2023-04-29 08:40:09 +0700

    change recording

--- x/meter/keeper/query_listrecordings.go
+++ x/meter/keeper/query_listrecordings.go
@@ -16,8 +16,52 @@ func (k Keeper) Listrecordings(goCtx context.Context, req *types.QueryListrecord
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Process the query
-	_ = ctx
+	// DONE: Process the query
+	// Performance Management
+	start := time.Now()
 
-	return &types.QueryListrecordingsResponse{}, nil
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	var meterreadingss []types.Meterreadings
+	var total uint64 = 0
+	// Define a variable from request
+	var mydeviceID string = req.DeviceID
+	var from uint64 = req.Start
+	var to uint64 = req.End
+	//var byblock bool = req.Byblock
+	var stDebug string = ""
+	//if(byblock==false) { stDebug .= " by timestamp "} else  { stDebug .= " by block "}
+
+	store := ctx.KVStore(k.storeKey)
+	meterreadingsStore := prefix.NewStore(store, types.KeyPrefix(types.MeterreadingsKeyPrefix))
+
+	pageRes, err := query.Paginate(meterreadingsStore, req.Pagination, func(key []byte, value []byte) error {
+		var meterreadings types.Meterreadings
+		if err := k.cdc.Unmarshal(value, &meterreadings); err != nil {
+			return err
+		}
+		if (mydeviceID == meterreadings.DeviceID) && (uint64(meterreadings.Timestamp) >= uint64(from)) && (uint64(meterreadings.Timestamp) <= uint64(to)) {
+			meterreadingss = append(meterreadingss, meterreadings)
+			total++
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	// Convert to array of json string
+	var displaylines []string 
+	for _, line := range meterreadingss {
+		json, _ := json.Marshal(line)
+		displaylines = append(displaylines, string(json))
+	}
+
+	elapsed := time.Since(start)
+	stDebug = fmt.Sprintf("Search took %s", elapsed)
+	return &types.QueryListrecordingsResponse{Meterreadings: displaylines, Comments: stDebug, Total: total, Pagination: pageRes}, nil
+	// return &types.QueryListrecordingsResponse{}, nil
 }
